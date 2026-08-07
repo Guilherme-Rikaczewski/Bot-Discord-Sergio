69b79bcc02293d06c01023d94f6b4c249eff50a8
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-08-06 22:46:02 -0300

    feat: adiciona validações do bot e do corpo da requisição

--- src/routes/webhook.py
+++ src/routes/webhook.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
 from src.schemas.discord import (
     DiscordInteraction,
     DiscordInteractionResponse,
@@ -6,14 +6,27 @@ from src.schemas.discord import (
 )
 from src.services.dispatcher import CommandDispatcher
 from src.utils.response_builder import ResponseBuilder
+from src.utils.validator import verify_discord_signature
 
-
-router = APIRouter(prefix="webhook", tags=["Webhook"])
+router = APIRouter(prefix="/webhook", tags=["Webhook"])
 dispatcher = CommandDispatcher()
 
 
 @router.post('/', response_model=DiscordInteractionResponse)
-async def webhook(interaction: DiscordInteraction):
+async def webhook(request: Request):
+    body = await request.body()
+
+    signature = request.headers["X-Signature-Ed25519"]
+    timestamp = request.headers["X-Signature-Timestamp"]
+
+    if not signature or not timestamp:
+        raise HTTPException(401)
+
+    if not verify_discord_signature(signature, timestamp, body):
+        raise HTTPException(401)
+
+    interaction = DiscordInteraction.model_validate_json(body)
+
     if interaction.type == InteractionType.PING:
         return ResponseBuilder.pong()
 

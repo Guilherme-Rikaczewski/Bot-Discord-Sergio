df7ae0403b5124f282fd71670e4521f21acd6e9b
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-08-06 22:43:46 -0300

    feat: adiciona resposta de teste para o service de rolagem de dados

--- src/services/dices.py
+++ src/services/dices.py
@@ -2,8 +2,11 @@ from src.schemas.discord import (
     DiscordInteraction,
 )
 from src.services.base_service import BaseService
+from src.utils.response_builder import ResponseBuilder
+from random import randint
 
 
 class DiceService(BaseService):
     async def execute(self, interaction: DiscordInteraction):
-        pass
+        print("teste")
+        return ResponseBuilder.message(f"Rolando 1d20: {randint(1, 20)}")
